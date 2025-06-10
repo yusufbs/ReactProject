abae836c5cd15610b3d9ad8bc958da9ece5d67de
Yusuf Samiwala <yusufbs@hotmail.com>
2025-06-10 16:07:34 -0500

    todo app working

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./componnts/Header";
+import Todo from "./componnts/Todo";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
---
+++ todoapp/src/componnts/Todo.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div>
+      <Form todos={todos} setTodos={setTodos}></Form>
+      <TodoList todos={todos} setTodos={setTodos}></TodoList>
+    </div>
+  );
+}
