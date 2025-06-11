156551f6c1cff3f55ecce8bb0ccdc2e148d1da16
Yusuf Samiwala <yusufbs@hotmail.com>
2025-06-10 20:12:12 -0500

    todo app complete

--- todoapp/src/componnts/Todo.jsx
+++ todoapp/src/componnts/Todo.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./footer";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length;
 
   return (
     <div>
       <Form todos={todos} setTodos={setTodos}></Form>
       <TodoList todos={todos} setTodos={setTodos}></TodoList>
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos}></Footer>
     </div>
   );
 }
