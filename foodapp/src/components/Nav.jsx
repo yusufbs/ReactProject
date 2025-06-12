30e32089fac24c20308645ae1e8168333b6681a7
Yusuf Samiwala <yusufbs@hotmail.com>
2025-06-12 06:14:09 -0500

    food app work in progress

---
+++ foodapp/src/components/Nav.jsx
@@ -0,0 +1,3 @@
+export default function Nav() {
+  return <div>🍔 Food App</div>;
+}
---
+++ foodapp/src/components/Search.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "67ef97f6cd6d40e2b7d670e1fcc61f19";
+
+//'https://api.spoonacular.com/recipes/complexSearch?query=pizza&apiKey=67ef97f6cd6d40e2b7d670e1fcc61f19'
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        type="text"
+      />
+    </div>
+  );
+}
